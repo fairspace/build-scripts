2692e713c51c1d692e5bde732f9cdb393c0505c1
Robert Horlings <robert@isdat.nl>
2018-10-19 11:35:49 +0200

    Add commit-id to snapshot version

--- setup_env.sh
+++ setup_env.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+export CURRENTVERSION="$(cat VERSION)"
+
 if [[ "$TRAVIS_BRANCH" = "$SNAPSHOT_BRANCH" ]]; then
-    export VERSION_POSTFIX="-SNAPSHOT"
+    # Set the commit id to either the actual commit hash or to the tag for actual releases
+    export COMMIT_ID=$(git log --pretty=format:'%h' -n 1)
+    export VERSION_POSTFIX="-SNAPSHOT-${COMMIT_ID}"
+else
+    export COMMIT_ID="v${CURRENTVERSION}"
 fi
 
 if [[ "$TRAVIS_BRANCH" = "$SNAPSHOT_BRANCH" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
@@ -9,17 +15,11 @@ if [[ "$TRAVIS_BRANCH" = "$SNAPSHOT_BRANCH" ]] && [[ "$TRAVIS_PULL_REQUEST" = "f
 fi
 
 if [[ "$TRAVIS_BRANCH" = "$RELEASE_BRANCH" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
+    export COMMIT_ID="v${CURRENTVERSION}"
     export SHOULD_RELEASE=1
 fi
 
-export CURRENTVERSION="$(cat VERSION)"
 export VERSION="${CURRENTVERSION}${VERSION_POSTFIX}"
 export CONTAINER_NAME="${DOCKER_REPO}/${ORG}/${APPNAME}:${VERSION}"
 export NEWVERSION="$(./ci/versioning/nextversion.sh ${CURRENTVERSION})"
 
-# Set the commit id to either the actual commit hash or to the tag for actual releases
-if [[ "$TRAVIS_BRANCH" = "$SNAPSHOT_BRANCH" ]]; then
-    export COMMIT_ID=$(git log --pretty=format:'%h' -n 1)
-else
-    export COMMIT_ID="v${CURRENTVERSION}"
-fi;
