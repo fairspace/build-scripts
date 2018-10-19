f0235cc00276eea773715c4b86a7f059ae56c96b
Robert Horlings <robert@isdat.nl>
2018-10-19 11:52:49 +0200

    Remove snapshot version postfix as problem is with caching

--- setup_env.sh
+++ setup_env.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 
-export CURRENTVERSION="$(cat VERSION)"
-
 if [[ "$TRAVIS_BRANCH" = "$SNAPSHOT_BRANCH" ]]; then
-    # Set the commit id to either the actual commit hash or to the tag for actual releases
-    export COMMIT_ID=$(git log --pretty=format:'%h' -n 1)
-    export VERSION_POSTFIX="-SNAPSHOT-${COMMIT_ID}"
-else
-    export COMMIT_ID="v${CURRENTVERSION}"
+    export VERSION_POSTFIX="-SNAPSHOT"
 fi
 
 if [[ "$TRAVIS_BRANCH" = "$SNAPSHOT_BRANCH" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
@@ -15,11 +9,17 @@ if [[ "$TRAVIS_BRANCH" = "$SNAPSHOT_BRANCH" ]] && [[ "$TRAVIS_PULL_REQUEST" = "f
 fi
 
 if [[ "$TRAVIS_BRANCH" = "$RELEASE_BRANCH" ]] && [[ "$TRAVIS_PULL_REQUEST" = "false" ]]; then
-    export COMMIT_ID="v${CURRENTVERSION}"
     export SHOULD_RELEASE=1
 fi
 
+export CURRENTVERSION="$(cat VERSION)"
 export VERSION="${CURRENTVERSION}${VERSION_POSTFIX}"
 export CONTAINER_NAME="${DOCKER_REPO}/${ORG}/${APPNAME}:${VERSION}"
 export NEWVERSION="$(./ci/versioning/nextversion.sh ${CURRENTVERSION})"
 
+# Set the commit id to either the actual commit hash or to the tag for actual releases
+if [[ "$TRAVIS_BRANCH" = "$SNAPSHOT_BRANCH" ]]; then
+    export COMMIT_ID=$(git log --pretty=format:'%h' -n 1)
+else
+    export COMMIT_ID="v${CURRENTVERSION}"
+fi;
