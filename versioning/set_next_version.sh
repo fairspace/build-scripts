f562a99124ebd92dffdc015f2d1bd68b41cc4458
Robert Horlings <robert@isdat.nl>
2018-10-19 16:14:57 +0200

    Set new version in develop branch after releasing

--- versioning/set_next_version.sh
+++ versioning/set_next_version.sh
@@ -11,6 +11,12 @@
 # Go back to the branch itself
 git stash --all
 git checkout $TRAVIS_BRANCH
+git pull
+
+# Ensure the latest code is on the snapshot branch as well
+git checkout $SNAPSHOT_BRANCH
+git pull
+git merge $TRAVIS_BRANCH --no-edit  
 
 # Set new version
 echo $NEWVERSION > VERSION
@@ -18,4 +24,5 @@ git add VERSION
 git commit -m "Prepare for next development cycle for version $NEWVERSION [skip ci]"
 git remote add origin-authenticated $(git remote get-url origin | sed s/github.com/$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/i)
 git push origin-authenticated HEAD
+
 
