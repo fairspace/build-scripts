667ac3a9ccb46669064d9a26e39a8f7b6c6ad62b
Robert Horlings <robert@isdat.nl>
2018-10-23 09:15:22 +0200

    Fix helm release script to work with or without existing charts

--- helm/release.sh
+++ helm/release.sh
@@ -11,16 +11,23 @@
 # Package chart
 helm package charts/${APPNAME}
 
-# Get Chart version number
-CHARTVERSION=$(sed -n 's/^version: //p' charts/${APPNAME}/Chart.yaml)
-echo Chart version number is $CHARTVERSION
+# Add chart to helm repo. Azure allows two options: --force for updating
+# and no option for adding.
+# For that reason, check whether the chart exists first for SNAPSHOT versions
+# non-snapshot versions should never be overwritten
+if [[ $VERSION == *SNAPSHOT ]]; then
+    az acr helm show $APPNAME --version $VERSION -n fairspace > /dev/null 2>&1
 
-# Add chart to helm repo
-if [[ $CHARTVERSION == *SNAPSHOT ]]; then
-    PUSH_PARAMS="--force"
+    # If the exit status is not 0, the chart does not exist yet
+    # If the exit status is 0, the chart does exist already
+    rc=$?
+    if [[ $rc == 0 ]]; then
+        echo "Chart version $VERSION already exists. Overwriting it"
+        DEPLOY_OPTIONS="--force"
+    fi
 fi
 
-az acr helm push ${APPNAME}-${CHARTVERSION}.tgz -n fairspace $PUSH_PARAMS
+az acr helm push ${APPNAME}-${VERSION}.tgz -n fairspace $DEPLOY_OPTIONS > /dev/null
 
 # Cleanup
 rm -rf $APPNAME*.tgz
