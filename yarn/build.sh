8859818b3c2ea4db2830218cae6ec5542fe85a5d
Robert Horlings <robert@isdat.nl>
2018-10-24 11:55:59 +0200

    Exit yarn build script on test error

--- yarn/build.sh
+++ yarn/build.sh
@@ -3,6 +3,6 @@
 # The environment variables are set are due to the version of gcc
 # needing to be at least 4.9 to properly install the
 # jsonld dependency rdf-canonize.
-env CC=/usr/bin/gcc-4.9 CXX=/usr/bin/g++-4.9 yarn install
-CI=true DISPLAY=:99 yarn test --silent
+env CC=/usr/bin/gcc-4.9 CXX=/usr/bin/g++-4.9 yarn install || exit $?
+CI=true DISPLAY=:99 yarn test --silent || exit $?
 yarn build
