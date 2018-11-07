b8538efaf461f01c1af08da9da2f1d81a0de2a63
Pavel Mikhailovskii <pavel@thehyve.nl>
2018-11-07 15:19:34 +0100

    Increase helm upgrade timeout

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -3,4 +3,4 @@
 ENVIRONMENT=${1:-ci}
 
 helm repo update
-helm upgrade --install ${APPNAME}-${ENVIRONMENT} fairspace/${APPNAME} --namespace=${APPNAME}-${ENVIRONMENT} --version $VERSION --timeout 600 -f ${DEPLOYMENT_CONFIG_DIR}/${ENVIRONMENT}-values.yaml
+helm upgrade --install ${APPNAME}-${ENVIRONMENT} fairspace/${APPNAME} --namespace=${APPNAME}-${ENVIRONMENT} --version $VERSION --timeout 1200 -f ${DEPLOYMENT_CONFIG_DIR}/${ENVIRONMENT}-values.yaml
