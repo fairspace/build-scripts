fc0a6400b3c209505d8ff07f4242174b3883bde4
Sietse Snel <sietse@thehyve.nl>
2018-11-19 09:53:30 +0100

    add_tag_to_git: add parameter for files to add

--- versioning/add_tag_to_git.sh
+++ versioning/add_tag_to_git.sh
@@ -8,9 +8,20 @@
 #   $GITHUB_USERNAME
 #   $GITHUB_PASSWORD
 #
+# Optional: $GIT_TAG_FILES_OVERRIDE
+#
 
 # Commit the updated values and put a git tag
-git add ./charts/$APPNAME/Chart.yaml ./charts/$APPNAME/values.yaml ${ARTIFACT_BUILD_FILE}
+
+if [ -z "$GIT_TAG_FILES_OVERRIDE" ]
+then GIT_TAG_FILES="./charts/${APPNAME}/Chart.yaml ./charts/${APPNAME}/values.yaml ${ARTIFACT_BUILD_FILE}"
+else GIT_TAG_FILES="$GIT_TAG_FILES_OVERRIDE"
+fi
+
+if [ ! -z "$GIT_TAG_FILES" ]
+then git add $GIT_TAG_FILES
+fi
+
 git commit -m "Release version $VERSION [skip ci]" --allow-empty # if first release then no verion update is performed
 git tag -fa v$VERSION -m "Release version $VERSION [skip ci]"
 git remote add origin-authenticated $(git remote get-url origin | sed s/github.com/$GITHUB_USERNAME:$GITHUB_PASSWORD@github.com/i)
