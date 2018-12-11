3fee751824cb3f28e9479263ed12acc0a25129bf
Robert Horlings <robert@isdat.nl>
2018-12-11 11:23:08 +0100

    Revert "Make helm install script more robust"
    This reverts commit f35d187

--- helm/install_helm.sh
+++ helm/install_helm.sh
@@ -3,19 +3,9 @@
 # Required env variables:
 #   $HELM_VERSION
 
-set -e
-set -u
-
-DOWNLOADDIR="$HOME/downloads"
-DOWNLOADFILE="$DOWNLOADDIR/helm-v${HELM_VERSION}.tar.gz"
-
-mkdir -p "$DOWNLOADDIR"
-
-if [ ! -f "$DOWNLOADFILE" ]
-then wget --no-clobber "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz" -O "$DOWNLOADFILE"
-fi
-
-tar xvfz "$DOWNLOADFILE"
+mkdir -p $HOME/downloads
+wget --no-clobber "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz" -O $HOME/downloads/helm-v${HELM_VERSION}.tar.gz
+tar xvfz $HOME/downloads/helm-v${HELM_VERSION}.tar.gz
 
 export PATH="$(pwd)/linux-amd64/:$PATH"
 helm init --client-only
