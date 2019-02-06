fd2d0b1d0e44e5b6288c97c327cb058799e859f2
Sietse Snel <sietse@thehyve.nl>
2019-02-06 18:25:54 +0100

    Simplify Kubernetes credential config for GCP
    Just retrieve the credentials via gcloud, so that we
    do not have to store an encrypted copy of the
    Kubernetes credentials for GCP clusters in the
    repositories.

--- k8s/config_kubectl.sh
+++ k8s/config_kubectl.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 #
-# Required env variables:
+# Required env variables for Azure:
 #
 # $KUBE_CONFIG_ENC_KEY
 # $KUBE_CONFIG_ENC_IV
 # $DEPLOYMENT_CONFIG_DIR
 #
+# Required env variables for GCP:
+# $CLUSTER_NAME
+# $CLUSTER_ZONE
+#
 # The specific kube-config file is stored encrypted
 #
-mkdir -p $HOME/.kube
-openssl aes-256-cbc -K $KUBE_CONFIG_ENC_KEY -iv $KUBE_CONFIG_ENC_IV -in ${DEPLOYMENT_CONFIG_DIR}/kube-config.yml.enc -out $HOME/.kube/config -d
+mkdir -p "$HOME/.kube"
+if [ "$DEPLOY_PLATFORM" = "GCP" ]
+then  echo "Retrieving Kubernetes credentials via gcloud ..."
+      gcloud container clusters get-credentials "$CLUSTER_NAME" --zone "$CLUSTER_ZONE"
+else  echo "Retrieving stored Kubernetes credentials for Azure ..."
+      openssl aes-256-cbc -K "$KUBE_CONFIG_ENC_KEY" -iv "$KUBE_CONFIG_ENC_IV" -in "${DEPLOYMENT_CONFIG_DIR}/kube-config.yml.enc" -out "$HOME/.kube/config" -d
+fi
