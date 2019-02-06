c3f4940ca75fb3ba88c94d9f59b8b81e250a0267
Sietse Snel <sietse@thehyve.nl>
2019-02-06 20:11:17 +0100

    helm/deploy: add workaround helm GCS plugin
    For not being able to set service account directly on GCP.

--- helm/deploy.sh
+++ helm/deploy.sh
@@ -2,5 +2,11 @@
 
 ENVIRONMENT=${1:-ci}
 
+if [ "$DEPLOY_PLATFORM" = "GCP" ]
+then # This is a workaround for not being able to provide a GCP service account
+     # for the GCS plugin directly. See https://github.com/nouney/helm-gcs/issues/12
+     export GOOGLE_APPLICATION_CREDENTIALS=/home/travis/ci-service-account.json
+fi
+
 helm repo update
-helm upgrade --install ${APPNAME}-${ENVIRONMENT} fairspace/${APPNAME} --namespace=${APPNAME}-${ENVIRONMENT} --version $VERSION -f ${DEPLOYMENT_CONFIG_DIR}/${ENVIRONMENT}-values.yaml --timeout 1500
+helm upgrade --install "${APPNAME}-${ENVIRONMENT}" "fairspace/${APPNAME}" "--namespace=${APPNAME}-${ENVIRONMENT}" --version "$VERSION" -f "${DEPLOYMENT_CONFIG_DIR}/${ENVIRONMENT}-values.yaml" --timeout 1500
