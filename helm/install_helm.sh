dc6d13f011739372eba762cab76cd7d92e083394
Sietse Snel <sietse@thehyve.nl>
2019-02-07 13:52:30 +0100

    Helm install: fix missing option in tar cmd

--- helm/install_helm.sh
+++ helm/install_helm.sh
@@ -8,7 +8,7 @@
 
 mkdir -p "$HOME/downloads"
 wget --no-clobber "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz" -O "$HOME/downloads/helm-v${HELM_VERSION}.tar.gz"
-tar xvfz "$HOME/downloads/helm-v${HELM_VERSION}.tar.gz"
+tar xvfz "$HOME/downloads/helm-v${HELM_VERSION}.tar.gz" --directory "$HOME"
 
 export PATH="$HOME/linux-amd64/:$PATH"
 helm init --client-only
