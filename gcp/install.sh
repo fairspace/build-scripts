e9b776926c62d178474e85c32b25f37e45546f2e
Sietse Snel <sietse@thehyve.nl>
2019-02-07 09:28:09 +0100

    Move helm GCS plugin install to helm inst. script
    ... because the gcp/install.sh script could also be used in jobs that
    do not require helm.
    
    Also clean up some minor style issues in helm install script.

--- gcp/install.sh
+++ gcp/install.sh
@@ -6,5 +6,3 @@ echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" \
      | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update && sudo apt-get install -y google-cloud-sdk
-
-helm plugin install https://github.com/nouney/helm-gcs
--- helm/install_helm.sh
+++ helm/install_helm.sh
@@ -2,10 +2,18 @@
 #
 # Required env variables:
 #   $HELM_VERSION
+#
+# Optional env variables:
+#   $DEPLOY_PLATFORM
 
-mkdir -p $HOME/downloads
-wget --no-clobber "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz" -O $HOME/downloads/helm-v${HELM_VERSION}.tar.gz
-tar xvfz $HOME/downloads/helm-v${HELM_VERSION}.tar.gz
+mkdir -p "$HOME/downloads"
+wget --no-clobber "https://storage.googleapis.com/kubernetes-helm/helm-v${HELM_VERSION}-linux-amd64.tar.gz" -O "$HOME/downloads/helm-v${HELM_VERSION}.tar.gz"
+tar xvfz "$HOME/downloads/helm-v${HELM_VERSION}.tar.gz"
 
-export PATH="$(pwd)/linux-amd64/:$PATH"
+export PATH="$HOME/linux-amd64/:$PATH"
 helm init --client-only
+
+if [ "$DEPLOY_PLATFORM" = "GCP" ]
+then  echo "Installing Helm GCS plugin ..."
+      helm plugin install https://github.com/nouney/helm-gcs
+fi
