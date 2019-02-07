443c510790607024bf8ec6957e5f243289ba8a32
Sietse Snel <sietse@thehyve.nl>
2019-02-07 15:01:07 +0100

    helm/add_repos: add workaround for GCS issue
    See comment for rationale

--- helm/add_repos.sh
+++ helm/add_repos.sh
@@ -4,6 +4,9 @@ helm repo add jupyterhub https://jupyterhub.github.io/helm-chart/
 
 if [ "$DEPLOY_PLATFORM" = "GCP" ]
 then  echo "Adding GCP helm repository ..."
+      # This is a workaround for not being able to provide a GCP service account
+      # for the GCS plugin directly. See https://github.com/nouney/helm-gcs/issues/12
+      export GOOGLE_APPLICATION_CREDENTIALS=/home/travis/ci-service-account.json
       helm repo add fairspace gs://fairspace-helm
 fi
 
