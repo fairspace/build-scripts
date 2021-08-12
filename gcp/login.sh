20e1909f7c850b22084757086f277144cbceb12b
ewelinagr <ewelina@thehyve.nl>
2021-08-12 11:36:10 +0200

    Update variable used for account config location.

--- gcp/login.sh
+++ gcp/login.sh
@@ -2,7 +2,7 @@
 #
 gcloud config set project "${GCP_PROJECT}"
 
-openssl aes-256-cbc -K "$KUBE_CONFIG_ENC_KEY" -iv "$KUBE_CONFIG_ENC_IV" -in "${DEPLOYMENT_CONFIG_DIR}/ci-service-account.json.enc" -out "$HOME/ci-service-account.json" -d
+openssl aes-256-cbc -K "$KUBE_CONFIG_ENC_KEY" -iv "$KUBE_CONFIG_ENC_IV" -in "${ACCOUNT_CONFIG_DIR}/ci-service-account.json.enc" -out "$HOME/ci-service-account.json" -d
 gcloud auth activate-service-account "--key-file=$HOME/ci-service-account.json"
 
 gcloud auth configure-docker -q
