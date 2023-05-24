9df1e7b14cb823994f8dd0a86ef3ae3ba62d2fbe
Ewelina Grudzień <ewelina@thehyve.nl>
2023-05-24 12:55:45 +0200

    Update install.sh

--- gcp/install.sh
+++ gcp/install.sh
@@ -5,4 +5,4 @@ CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb https://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" \
      | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-snap install google-cloud-sdk --classic
+sudo snap install google-cloud-sdk --classic
